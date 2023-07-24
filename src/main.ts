983c9757595e3288d9d0e71c691639d91d7110b9
Dzmitry Halavach <kotik1@protonmail.com>
2023-07-24 11:52:43 +0300

    feat: user logic

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { readFile } from 'fs/promises';
+import { dirname } from 'path';
+import { parse } from 'yaml';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const api = await readFile(
+    join(dirname(__dirname), 'doc', 'api.yaml'),
+    'utf-8',
+  );
+  const doc: OpenAPIObject = parse(api);
+  SwaggerModule.setup('doc', app, doc);
+  await app.listen(5000);
 }
 bootstrap();
