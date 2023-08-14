8bc9f60340e8b179b6b84847978174fe150fd8f5
Dzmitry Halavach <kotik1@protonmail.com>
2023-08-15 00:18:41 +0300

    feat: add custom exception filter to main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,9 @@ import { dirname } from 'path';
 import { parse } from 'yaml';
 import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import { Logger } from './log/logging-service';
-import { ValidationPipe } from '@nestjs/common';
+import { HttpException, ValidationPipe } from '@nestjs/common';
 import { config } from 'dotenv';
+import { HttpExceptionFilter } from './exceptions/http-exception-filter';
 
 config();
 const port = Number(process.env.PORT) || 5000;
@@ -19,6 +20,7 @@ async function bootstrap() {
     rawBody: true,
   });
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   const api = await readFile(
     join(dirname(__dirname), 'doc', 'api.yaml'),
